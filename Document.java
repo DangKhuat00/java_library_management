61da8992a08ca53dadd5a4148fcec54b403db1dc
Vu Thanh Tung <vutung332@gmail.com>
2025-07-20 00:43:36 +0700

    dinh nghia class Document

--- Document.java
+++ Document.java
@@ -1,7 +1,18 @@
 package librarymanage.java_library_management;
 
 public class Document {
-    
+    private String id;
+    private String title;
+    private String author;
+    private String publisher;
+    private int year;
+    private String category;
+    private int numbers;
+    private boolean isAvailable;
+
+    public static void main(String[] args) {
+
+    }
 
 
 }
